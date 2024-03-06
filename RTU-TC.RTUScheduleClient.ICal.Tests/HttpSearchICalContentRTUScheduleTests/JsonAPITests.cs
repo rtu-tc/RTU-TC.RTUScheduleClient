29443ede5ec8d44113d9ad5dd00bc21237a15c85
Maksim Makushchenko <maksalmak@gmail.com>
2024-03-07 01:37:07 +0300

    Setup test for pages iterate

--- RTU-TC.RTUScheduleClient.ICal.Tests/HttpSearchICalContentRTUScheduleTests/JsonAPITests.cs
+++ RTU-TC.RTUScheduleClient.ICal.Tests/HttpSearchICalContentRTUScheduleTests/JsonAPITests.cs
@@ -2,7 +2,7 @@
 using RichardSzalay.MockHttp;
 
 namespace RTU_TC.RTUScheduleClient.ICal.Tests.HttpSearchICalContentRTUScheduleTests;
-public class CorrectParseResponseTests
+public class JsonAPITests
 {
     [Fact]
     public async Task NoSchedulesIsEmptyCollection()
@@ -52,5 +52,52 @@ public class CorrectParseResponseTests
         schedule.TargetTitle.Should().Be("2 (С-20)");
         schedule.ScheduleTarget.Should().Be(ScheduleTarget.Auditorium);
     }
+
+
+    [Fact]
+    public async Task TwoPagesHandleCorrect()
+    {
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.When("http://localhost/schedule/api/search")
+                .WithQueryString("pageToken", "")
+                .Respond("application/json", """
+                { "data" : [ {
+                    "id": 1,
+                    "targetTitle": "2 (С-20)",
+                    "scheduleTarget": 3,
+                    "iCalLink": "ignore-it",
+                    "scheduleImageLink": "ignore-it",
+                    "scheduleUpdateImageLink": "ignore-it"
+                } ],
+                "nextPageToken": "tokenValue"
+                }
+                """);
+
+        mockHttp.When("http://localhost/schedule/api/search")
+                .WithQueryString("pageToken", "tokenValue")
+                .Respond("application/json", """
+                { "data" : [ {
+                    "id": 2,
+                    "targetTitle": "Группа",
+                    "scheduleTarget": 1,
+                    "iCalLink": "ignore-it",
+                    "scheduleImageLink": "ignore-it",
+                    "scheduleUpdateImageLink": "ignore-it"
+                } ],
+                "nextPageToken": null
+                }
+                """);
+
+        var client = mockHttp.ToHttpClient();
+        client.BaseAddress = new Uri("http://localhost");
+        client.DefaultRequestHeaders.Add(HttpSearchICalContentRTUSchedule.ClientNameHeaderKey, "tests");
+        var scheduleClient = new HttpSearchICalContentRTUSchedule(client);
+
+        var schedules = await scheduleClient.GetAllSchedulesAsync().ToArrayAsync();
+
+        schedules.Should().HaveCount(2);
+    }
+
 
 }
